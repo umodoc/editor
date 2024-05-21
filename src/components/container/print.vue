4bf396f7db41f92496ee3452d377df093158fc91
umodoc <team@umodoc.com>
2024-05-21 16:11:47 +0800

    页面预览和打印添加支持数学公式

--- src/components/container/print.vue
+++ src/components/container/print.vue
@@ -37,7 +37,9 @@
 </template>
 
 <script setup>
-const { container, editor, page, printing } = useStore()
+import katex from 'katex'
+
+const { container, options, page, printing } = useStore()
 const $document = useState('document')
 let dialogVisible = $ref(false)
 let pageOptionsVisible = $ref(false)
@@ -63,7 +65,8 @@ let iframeCode = $ref('')
 
 const getIframeCode = () => {
   if (!dialogVisible) return ''
-  editor.value.commands.blur()
+
+  // 获取页面内容并进行整理和清洗
   let pageContent = document
     .querySelector(`${container} .umo-editor`)
     .cloneNode(true)
@@ -102,11 +105,35 @@ const getIframeCode = () => {
         '<div class="page-break"></div><div class="page-break"></div>'
     }
   })
-  const { orientation, size, margin, background } = page.value
+
+  // 数学公式
+  const mathematics = pageContent.querySelectorAll('.Tiptap-mathematics-editor')
+  if (mathematics) {
+    mathematics.forEach((item) => {
+      const text = item.textContent
+      if (text.length > 2) {
+        item.classList.remove('Tiptap-mathematics-editor--hidden')
+        item.setAttribute('style', '')
+        item.innerHTML = katex.renderToString(text.slice(1, -1), {
+          output: 'html',
+        })
+      }
+    })
+  }
+  const mathRender = pageContent.querySelectorAll('.Tiptap-mathematics-render')
+  if (mathRender) {
+    mathRender.forEach((item) => item.parentNode.removeChild(item))
+  }
+
+  // 页面水印
   const watermark = document
     .querySelector('.umo-watermark')
     .lastChild.cloneNode(true)
   watermark.classList.add('page-watermark')
+
+  // 页面属性
+  const { orientation, size, margin, background } = page.value
+
   return `
     <!DOCTYPE html>
     <html lang="zh-CN">
@@ -114,15 +141,117 @@ const getIframeCode = () => {
       <title>${$document.value.title}</title>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script src="/libs/paged.polyfill.min.js"><\/script>
-      <link href="/dist/umd/style.css?v=${new Date().getTime()}" rel="stylesheet" />
-      <link href="/print.css" rel="stylesheet" />
+      <script src="${options.value.cdnUrl}/libs/paged.polyfill.min.js"><\/script>
+      <link href="/dist/umd/style.css" rel="stylesheet" />
+      <link href="${options.value.cdnUrl}/libs/katex/katex.min.css" rel="stylesheet" />
       <style>
-        @page {
-          size: ${orientation === 'horizontal' ? size.width : size.height}cm ${orientation === 'horizontal' ? size.height : size.width}cm; 
+      body {
+        background-color: var(--umo-container-background);
+        padding: 25px;
+        box-sizing: border-box;
+        margin: 0;
+      }
+      body.loading {
+        overflow: hidden;
+        position: relative;
+        height: 100vh;
+        padding: 0;
+      }
+      body.loading:after {
+        content: '正在加载中...';
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #999;
+        font-size: 12px;
+        background-color: var(--umo-container-background);
+        z-index: 9999;
+      }
+      body.printing {
+        padding: 0;
+      }
+
+      /* 文档节点 */
+      .umo-editor {
+        position: relative;
+        word-wrap: break-word;
+        white-space: pre-wrap;
+        white-space: break-spaces;
+        -webkit-font-variant-ligatures: none;
+        font-variant-ligatures: none;
+        font-feature-settings: 'liga' 0;
+      }
+      .umo-editor > .is-empty,
+      .ProseMirror-selectednode,
+      .Tiptap-invisible-character,
+      .node-view .uploading,
+      .select-outline.file .file-action,
+      .node-view .code-block .show-toolbar,
+      .node-view .uploading,
+      .selectedCell::after,
+      .prism-code-editor .guide-indents,
+      .prism-code-editor .active-line::after,
+      .katex-mathml {
+        display: none !important;
+      }
+
+      .hover-shadow {
+        box-shadow: none !important;
+      }
+      .select-outline {
+        outline: none !important;
+      }
+      .select-outline.code-block,
+      .select-outline.iframe,
+      .select-outline.toc,
+      .select-outline.file,
+      .select-outline.audio {
+        outline: solid 1px var(--umo-content-node-border) !important;
+      }
+
+      .Tiptap-mathematics-editor {
+        background: none !important;
+      }
+
+      .page-break {
+        margin: 0 !important;
+        height: 0 !important;
+        overflow: hidden;
+      }
+
+      /* 打印样式 */
+      .pagedjs_pages {
+        display: flex;
+        align-content: center;
+        flex-wrap: wrap;
+        flex-direction: column;
+        gap: 15px;
+      }
+
+      @page {
+        size: ${orientation === 'horizontal' ? size.width : size.height}cm ${orientation === 'horizontal' ? size.height : size.width}cm; 
           margin: ${margin.top}cm ${margin.right}cm ${margin.bottom}cm ${margin.left}cm; 
           background: ${background};
+        box-shadow:
+          rgba(0, 0, 0, 0.06) 0px 0px 10px 0px,
+          rgba(0, 0, 0, 0.04) 0px 0px 0px 1px;
+        pointer-events: none;
+        @bottom-center {
+          content: '第 ' counter(page) ' 页 / 共 ' counter(pages) ' 页';
+          font-size: 12px;
+          color: #999;
         }
+      }
+
+      body.printing .pagedjs_page {
+        box-shadow: none !important;
+      }
       </style>
       <script>
       class Handler extends Paged.Handler {
@@ -168,7 +297,7 @@ const getIframeCode = () => {
     </html>`
 }
 
-// TODO: 代码缩进、数学公式
+// TODO: 代码缩进
 watch(
   () => dialogVisible,
   () => {
