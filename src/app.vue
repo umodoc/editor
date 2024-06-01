1b3e09d4d18617d8ac5c5e2b34ca9354cb5d61b5
umodoc <team@umodoc.com>
2024-06-01 11:57:17 +0800

    完善模板

--- src/app.vue
+++ src/app.vue
@@ -7,6 +7,18 @@
 <script setup>
 import generateId from '@/utils/generate-id'
 const editorRef = $ref(null)
+const templates = [
+  {
+    title: '工作任务',
+    description: '工作任务模板',
+    content: `<h1>工作任务</h1><h3>任务名称：</h3><p>[任务的简短描述]</p><h3>负责人：</h3><p>[执行任务的个人姓名]</p><h3>截止日期：</h3><p>[任务需要完成的日期]</p><h3>任务详情：</h3><ol><li>[任务步骤1]</li><li>[任务步骤2]</li><li>[任务步骤3]...</li></ol><h3>目标：</h3><p>[任务需要达成的具体目标或结果]</p><h3>备注：</h3><p>[任何额外信息或注意事项]</p>`,
+  },
+  {
+    title: '工作周报',
+    description: '工作周报模板',
+    content: `<h1>工作周报</h1><h2>本周工作总结</h2><hr /><h3>已完成工作：</h3><ul><li>[任务1名称]：[简要描述任务内容及完成情况]</li><li>[任务2名称]：[简要描述任务内容及完成情况]</li><li>...</li></ul><h3>进行中工作：</h3><ul><li>[任务1名称]：[简要描述任务当前进度和下一步计划]</li><li>[任务2名称]：[简要描述任务当前进度和下一步计划]</li><li>...</li></ul><h3>问题与挑战：</h3><ul><li>[问题1]：[描述遇到的问题及当前解决方案或需要的支持]</li><li>[问题2]：[描述遇到的问题及当前解决方案或需要的支持]</li><li>...</li></ul><hr /><h2>下周工作计划</h2><h3>计划开展工作：</h3><ul><li>[任务1名称]：[简要描述下周计划开始的任务内容]</li><li>[任务2名称]：[简要描述下周计划开始的任务内容]</li><li>...</li></ul><h3>需要支持与资源：</h3><ul><li>[资源1]：[描述需要的资源或支持]</li><li>[资源2]：[描述需要的资源或支持]</li><li>...</li></ul>`,
+  },
+]
 const onSave = async (content, page, document) => {
   localStorage.setItem('document.content', document.content)
   return new Promise((resolve, reject) => {
@@ -31,18 +43,7 @@ const options = $ref({
     // title: '测试文档',
     content: localStorage.getItem('document.content') || '<p>测试文档</p>',
   },
-  templates: [
-    {
-      title: '工作任务',
-      description: '工作任务模板',
-      content: '<p>工作任务</p>',
-    },
-    {
-      title: '工作周报',
-      description: '工作周报模板',
-      content: '<h2>工作任务</h2>',
-    },
-  ],
+  templates,
   shareUrl: 'https://umodoc.com',
   file: {
     // allowedMimeTypes: [
