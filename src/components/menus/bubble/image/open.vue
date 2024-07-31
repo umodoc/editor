ae0da7fa5dcf7c0c9a3d627219dc7ec20a67174d
umodoc <team@umodoc.com>
2024-07-31 13:28:09 +0800

    图片下载改为打开图片

---
+++ src/components/menus/bubble/image/open.vue
@@ -0,0 +1,21 @@
+<template>
+  <menus-button
+    ico="image-open"
+    :text="t('bubbleMenu.image.open')"
+    @menu-click="openImage"
+  />
+</template>
+
+<script setup>
+const { editor } = useStore()
+
+const openImage = () => {
+  const node = editor.value?.commands.getSelectionNode()
+  const a = document.createElement('a')
+  a.href = node.attrs.src
+  a.target = '_blank'
+  document.body.appendChild(a)
+  a.click()
+  document.body.removeChild(a)
+}
+</script>
