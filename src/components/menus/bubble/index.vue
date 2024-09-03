97686fa5e849fd8cbca5a63e1a2c3bb35054f414
Sunny Wisozk <37365363+ChenErik@users.noreply.github.com>
2024-09-03 12:59:34 +0800

    修复只读模式下图片依旧可以拖拽缩放的问题 (#62)
    * 添加缺少的依赖
    
    * 修复只读模式下图片依旧可以拖拽缩放的问题
    
    * 修复演讲模式无法打开的问题

--- src/components/menus/bubble/index.vue
+++ src/components/menus/bubble/index.vue
@@ -1,21 +1,16 @@
 <template>
-  <bubble-menu
-    v-show="!blockMenu && !painter.enabled && !editor.isEmpty"
-    class="umo-editor-bubble-menu"
-    :class="{ assistant: assistantBox }"
-    :editor="editor"
-    :tippy-options="tippyOpitons"
-  >
-    <menus-bubble-menus
-      v-if="options.document.enableBubbleMenu && !assistantBox && !commentBox"
-    >
-      <template #bubble_menu="props">
-        <slot name="bubble_menu" v-bind="props" />
-      </template>
-    </menus-bubble-menus>
-    <assistant-input v-if="options.assistant.enabled && assistantBox" />
-    <comment-input v-if="options.document.enableComment && commentBox" />
-  </bubble-menu>
+  <div>
+    <bubble-menu v-show="!blockMenu && !painter.enabled && !editor.isEmpty" class="umo-editor-bubble-menu"
+      :class="{ assistant: assistantBox }" :editor="editor" :tippy-options="tippyOpitons">
+      <menus-bubble-menus v-if="options.document.enableBubbleMenu && !assistantBox && !commentBox">
+        <template #bubble_menu="props">
+          <slot name="bubble_menu" v-bind="props" />
+        </template>
+      </menus-bubble-menus>
+      <assistant-input v-if="options.assistant.enabled && assistantBox" />
+      <comment-input v-if="options.document.enableComment && commentBox" />
+    </bubble-menu>
+  </div>
 </template>
 
 <script setup>
