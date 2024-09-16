421716a18c730346f8eb175dfa4b84b62778eee6
umodoc <team@umodoc.com>
2024-09-16 22:09:08 +0800

    优化

--- src/composables/node-postion.ts
+++ src/composables/node-postion.ts
@@ -1,8 +1,8 @@
-const { container } = useStore()
+const { container, editor } = useStore()
 
 export const useNodePostion = () => {
   const currentNode = document.querySelector(
-    `${container} .umo-page-node-content .umo-node-focused`,
+    `${container} .umo-page-node-content .umo-node-focused, ${container} .umo-page-node-content .ProseMirror-gapcursor`,
   ) as HTMLElement
   if (currentNode === null) {
     return { offsetTop: null, offsetLeft: null }
@@ -13,12 +13,16 @@ export const useNodePostion = () => {
 
   // 微修正菜单位置
   offsetTop = currentNode.tagName === 'DIV' ? offsetTop - 8 : offsetTop - 5
-  // let offsetY = 0
-  // if (
-  //   editor.value.isActive('horizontalRule') ||
-  //   editor.value.isActive('table')
-  // ) {
-  //   offsetY = 5
-  // }
+
+  if (currentNode.classList.contains('ProseMirror-gapcursor')) {
+    offsetTop = offsetTop + 5
+  }
+  if (
+    editor.value.isActive('horizontalRule') ||
+    editor.value.isActive('table') ||
+    currentNode.classList.contains('ProseMirror-gapcursor')
+  ) {
+    offsetTop = offsetTop + 5
+  }
   return { offsetTop, offsetLeft }
 }
