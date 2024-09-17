b1fc67bb08497d6d82eb6cdbf3d42d4b84dbd41b
umodoc <team@umodoc.com>
2024-09-17 13:23:27 +0800

    样式优化

--- src/utils/copyright.ts
+++ src/utils/copyright.ts
@@ -11,7 +11,7 @@ export default `/**
 export const consoleCopyright = () => {
   console.info(
     t('welcome', { version: pkg.version, homepage: pkg.homepage }),
-    'background:#3480f9;color:#fff;border-top-left-radius:3px;border-bottom-left-radius:3px;padding:4px 8px;',
-    'background:#fff;color:#3480f9;border-top-right-radius:3px;border-bottom-right-radius:3px;border:solid 1px #3480f9;padding:3px 8px;',
+    'background:#3480f9;color:#fff;border-top-left-radius:3px;border-bottom-left-radius:3px;padding:4px 8px 3px;',
+    'background:#fff;color:#3480f9;border-top-right-radius:3px;border-bottom-right-radius:3px;border:solid 1px #3480f9;padding:3px 8px 2px;',
   )
 }
