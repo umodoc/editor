b067794c582875715c85c4cbfdcd905e28231b42
umodoc <team@umodoc.com>
2025-02-22 17:49:12 +0800

    优化块级菜单等

--- src/components/menus/context/block/node.vue
+++ src/components/menus/context/block/node.vue
@@ -3,6 +3,7 @@
     :attach="`${container} .umo-page-container`"
     placement="bottom-right"
     overlay-class-name="umo-block-menu-dropdown"
+    max-height="320"
     trigger="click"
     :destroy-on-close="false"
     :popup-props="popupProps"
@@ -34,118 +35,104 @@
       </t-dropdown-item>
       <t-dropdown-item>
         <menus-button
-          ico="node-add"
-          :text="t('blockMenu.select')"
+          ico="table"
+          :text="t('table.insert.text')"
           :tooltip="false"
+          @menu-click="editor?.chain().focus().insertTable().run()"
         />
-        <t-dropdown-menu
-          overlay-class-name="umo-block-menu-dropdown"
-          placement="right"
-        >
-          <t-dropdown-item>
-            <menus-button
-              ico="table"
-              :text="t('table.insert.text')"
-              :tooltip="false"
-              @menu-click="editor?.chain().focus().insertTable().run()"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-image :huge="false" :tooltip="false" />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-video
-              v-if="!disableItem('video')"
-              :huge="false"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-audio
-              v-if="!disableItem('audio')"
-              :huge="false"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-file
-              v-if="!disableItem('file')"
-              :huge="false"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-code-block
-              v-if="!disableItem('code-block')"
-              :huge="false"
-              shortcut-text="Ctrl+Alt+C"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-button
-              ico="hr"
-              :text="t('insert.hr.text')"
-              :tooltip="false"
-              @menu-click="
-                editor?.chain().focus().setHr({ type: 'signle' }).run()
-              "
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-toc :huge="false" :tooltip="false" />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-text-box :huge="false" :tooltip="false" />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-insert-web-page :huge="false" :tooltip="false" />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-tools-qrcode :huge="false" :tooltip="false" />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-tools-barcode :huge="false" :tooltip="false" />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-tools-signature
-              v-if="!disableItem('signature')"
-              :huge="false"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-tools-seal
-              v-if="!disableItem('seal')"
-              :huge="false"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-tools-diagrams
-              v-if="!disableItem('diagrams')"
-              :huge="false"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-tools-echarts
-              v-if="!disableItem('echarts')"
-              :huge="false"
-              :tooltip="false"
-              mode="add"
-            />
-          </t-dropdown-item>
-          <t-dropdown-item>
-            <menus-toolbar-tools-mermaid
-              v-if="!disableItem('mermaid')"
-              :huge="false"
-              :tooltip="false"
-            />
-          </t-dropdown-item>
-        </t-dropdown-menu>
       </t-dropdown-item>
       <t-dropdown-item>
+        <menus-toolbar-insert-image :huge="false" :tooltip="false" />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-insert-video
+          v-if="!disableItem('video')"
+          :huge="false"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-insert-audio
+          v-if="!disableItem('audio')"
+          :huge="false"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-insert-file
+          v-if="!disableItem('file')"
+          :huge="false"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-insert-code-block
+          v-if="!disableItem('code-block')"
+          :huge="false"
+          shortcut-text="Ctrl+Alt+C"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-button
+          ico="hr"
+          :text="t('insert.hr.text')"
+          :tooltip="false"
+          @menu-click="editor?.chain().focus().setHr({ type: 'signle' }).run()"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-insert-toc :huge="false" :tooltip="false" />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-insert-text-box :huge="false" :tooltip="false" />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-insert-web-page :huge="false" :tooltip="false" />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-tools-qrcode :huge="false" :tooltip="false" />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-tools-barcode :huge="false" :tooltip="false" />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-tools-signature
+          v-if="!disableItem('signature')"
+          :huge="false"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-tools-seal
+          v-if="!disableItem('seal')"
+          :huge="false"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-tools-diagrams
+          v-if="!disableItem('diagrams')"
+          :huge="false"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-tools-echarts
+          v-if="!disableItem('echarts')"
+          :huge="false"
+          :tooltip="false"
+          mode="add"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item>
+        <menus-toolbar-tools-mermaid
+          v-if="!disableItem('mermaid')"
+          :huge="false"
+          :tooltip="false"
+        />
+      </t-dropdown-item>
+      <t-dropdown-item v-if="options.templates.length > 0">
         <menus-button
           ico="template"
           :text="t('blockMenu.template')"
