db52843756cc24499df5eaa2f2db61e7385985d2
王旭 <155091355+wanglaosanxu@users.noreply.github.com>
2025-03-03 01:16:11 +0800

    表格气泡窗口增加表格的居中居左居右
    表格气泡窗口增加表格的居中居左居右

---
+++ src/components/menus/toolbar/table/node-align.vue
@@ -0,0 +1,26 @@
+<template>
+  <menus-button
+    ico="table-node-align"
+    :text="t('table.nodeAlign.text')"
+    menu-type="dropdown"
+    huge
+    :select-options="alignments"
+    @click="setcurNodeAlign"
+  />
+</template>
+
+<script setup lang="ts">
+const editor = inject('editor')
+
+const alignments = [
+  { content: t('table.nodeAlign.left'), value: 'flex-start' },
+  { content: t('table.nodeAlign.center'), value: 'center' },
+  { content: t('table.nodeAlign.right'), value: 'flex-end' },
+]
+const setcurNodeAlign = ({ value }: { value: string }) => {
+  if (!value) {
+    return
+  }
+  editor.value.commands.setNodeAlign(value)
+}
+</script>
