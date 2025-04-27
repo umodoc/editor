05c7d6d6c52a8ba7d1f5b24bc240eda4a14a2874
china-wangxu <18366136571@163.com>
2025-04-27 12:01:33 +0800

    AI助手替换后自动给替换文本增加选中效果

--- src/components/ai/assistant/input.vue
+++ src/components/ai/assistant/input.vue
@@ -124,7 +124,7 @@
 <script setup lang="ts">
 import { isString } from '@tool-belt/type-predicates'
 
-import { getSelectionText } from '@/extensions/selection'
+import { getSelectionText, setSelectionText } from '@/extensions/selection'
 import type {
   AssistantContent,
   AssistantPayload,
@@ -237,23 +237,32 @@ const exitAssistant = () => {
 }
 
 const replaceContent = () => {
+  // 记录插入前的选区位置
+  const { from, to } = editor.value?.state.selection ?? {}
+  const prevDocLength = editor.value?.state.doc.content.size || 0
   editor.value?.chain().insertContent(result.content).run()
+  setSelectionText(editor.value, prevDocLength, from, to)
   exitAssistant()
 }
 
 const insertContentAtAfter = () => {
   const { to } = editor.value?.state.selection ?? {}
+  const prevDocLength = editor.value?.state.doc.content.size || 0
   if (to) {
     editor.value?.chain().insertContentAt(to, result.content).focus().run()
+    setSelectionText(editor.value, prevDocLength, to, to)
   }
+
   exitAssistant()
 }
 
 const insertContentAtBelow = () => {
   editor.value?.commands.selectParentNode()
   const { to } = editor.value?.state.selection ?? {}
+  const prevDocLength = editor.value?.state.doc.content.size || 0
   if (to) {
     editor.value?.chain().insertContentAt(to, result.content).focus().run()
+    setSelectionText(editor.value, prevDocLength, to, to)
   }
   exitAssistant()
 }
