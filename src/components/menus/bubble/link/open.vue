33fd8b51a51b4a6c05b5f98dd4e678022fbe297f
有墨文档 <contact@umodoc.com>
2025-05-18 10:39:43 +0800

    修复文档中的链接打开无效的问题

--- src/components/menus/bubble/link/open.vue
+++ src/components/menus/bubble/link/open.vue
@@ -1,8 +1,5 @@
 <template>
-  <menus-button
-    :text="t('insert.link.open')"
-    @menu-click="emits('hide-bubble')"
-  >
+  <menus-button :text="t('insert.link.open')" @menu-click="menuClick">
     <a class="umo-link" :href="editor.storage.link.meta.href" target="_blank"
       ><icon name="link" /> {{ t('insert.link.open') }}</a
     >
@@ -12,6 +9,12 @@
 <script setup lang="ts">
 const editor = inject('editor')
 const emits = defineEmits(['hide-bubble'])
+
+const menuClick = () => {
+  setTimeout(() => {
+    emits('hide-bubble')
+  }, 100)
+}
 </script>
 
 <style lang="less" scoped>
