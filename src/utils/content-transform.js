189361851199fed499bea5b6334c5a7c5de23196
有墨文档 <contact@umodoc.com>
2026-08-16 17:21:13 +0800

    细节调整

--- src/utils/content-transform.js
+++ src/utils/content-transform.js
@@ -1,6 +1,10 @@
 export const contentTransform = (content) => {
   // 处理空内容或非字符串内容
-  if (content && typeof content === 'string' && !content.startsWith('<')) {
+  if (
+    content &&
+    typeof content === 'string' &&
+    !content.trimStart().startsWith('<')
+  ) {
     // 处理纯文本中的换行符
     return content
       .split('\n')
